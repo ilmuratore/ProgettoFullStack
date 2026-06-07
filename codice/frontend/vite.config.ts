80d5802817f612f3c86a1be9ba43081cf7dffa7c
Simone Iengo <iengo.simone@gmail.com>
2026-06-07 17:08:50 +0200

    fix tunnell cors only dev purpose

--- codice/frontend/vite.config.ts
+++ codice/frontend/vite.config.ts
@@ -7,9 +7,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5173,
-    allowedHosts: [
-      'queensland-express-showers-belts.trycloudflare.com'
-    ],
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://backend:3000',
